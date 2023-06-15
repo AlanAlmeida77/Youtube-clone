b139a031c374836f07a679a1f3425187daf8f06e
Alan Almeida <alandavidalmeida@gmail.com>
2023-06-15 17:30:48 -0300

    components

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import {useState, useEffect } from 'react';
+import {Box, Stack, Typography } from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack>
+
+    </Stack>
   )
 }
 
